84b529242a22a00b433d4e8ddc8841c85c2f6385
Naseef Nusky <naseefnusky09@gmail.com>
2026-08-28 14:37:18 +0530

    minor changes-responsive improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import ScrollToTop from './components/ScrollToTop'
 import AppRoutes from './routes/AppRoutes'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
